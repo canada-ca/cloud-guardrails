03d3a3db69a9d40f57e4ab30b75bc51772a9c5cc
Nick Schonning <nschonni@gmail.com>
2019-12-07 23:21:44 -0500

    chore: Copy linting setup from template repo
    Minor formatting for Markdownlint flagging.
    Additional issues with files with capitalized MD that will be tackled after.

---
+++ link-check.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var markdownLinkCheck = require('markdown-link-check');
+var fs = require("fs");
+var glob = require("glob");
+var path = require("path");
+var chalk = require("chalk");
+
+
+var files = glob.sync("**/*.md", {ignore: ["node_modules/**/*.md"]})
+
+var config = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
+config.timeout = '30s'
+
+var opts = JSON.parse(fs.readFileSync(".markdown-link-check.json"));
+
+files.forEach(function(file) {
+  var markdown = fs.readFileSync(file).toString();
+  let opts = Object.assign({}, config);
+
+  opts.baseUrl = path.dirname(path.resolve(file)) + '/';
+
+  markdownLinkCheck(markdown, opts, function (err, results) {
+    if (err) {
+        console.error('Error', err);
+        return;
+    }
+
+    console.log(chalk.green("Reading: " + file));
+
+    results.forEach(function (result) {
+      if(result.status === "dead") {
+        if (result.statusCode == 500) {
+          console.log(chalk.yellow("Server error on target: " + result.link));
+        }
+        else {
+          process.exitCode = 1
+          console.log(chalk.red("Dead: " + result.link));
+        }
+      }
+    });
+  });
+});
